9ba678202cec5d34fa6398ecde04315b86f819e2
Trquangnhat <quangnhattruong34@gmail.com>
2022-08-29 20:04:40 +0700

    baitaptuan1

---
+++ tuan1/tuan1.rb
@@ -0,0 +1,14 @@
+p "Xin chào Trương Quang Nhật - MSV: 1911505310243"
+puts "Nhập số bất kỳ:"
+number = gets
+puts "Số vừa nhập là: #{number}"
+puts "So sánh 2 số"
+puts "Nhập A"
+a = gets
+puts "Nhập B"
+b = gets
+if a > b
+    puts "A lớn hơn B"
+else
+    puts "B lớn hơn A"
+end
